859652c85a6d6cb926c880ec51639d4de0addf36
s16948 <67000174+s16948@users.noreply.github.com>
2020-06-27 22:17:11 +0200

    Add files via upload
    Cwiczenie 1 i Cwiczenie 2

---
+++ Cw2/Cw2/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetStudents(Student student)
+        {
+          //  var st = new Student();
+           // st.idStudent = id;
+           // st.FirstName = "Jan";
+           // st.LastName = "Kowalski";
+            //st.LastName = orderBy;
+
+            return Ok(student);
+        }
+    }
+}
---
+++ Cw2/Cw2/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw2.Models
+{
+    public class Student
+    {
+
+        public int idStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
