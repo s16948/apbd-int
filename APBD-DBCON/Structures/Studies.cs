11cdf345155a2437bca9cae73d2f2c495f8a1f2a
s16948 <67000174+s16948@users.noreply.github.com>
2020-06-27 22:30:28 +0200

    Add files via upload

---
+++ APBD-DBCON/Structures/Studies.cs
@@ -0,0 +1,21 @@
+﻿namespace DeansOffice.Structures
+{
+    public class Studies
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+        public override bool Equals(object obj)
+        {
+            var toCheck = obj as Studies;
+            return Name == toCheck.Name;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
