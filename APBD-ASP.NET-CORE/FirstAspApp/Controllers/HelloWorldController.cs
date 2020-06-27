61afd21c0f341907e022bac91a9334a3fa2e3174
s16948 <67000174+s16948@users.noreply.github.com>
2020-06-27 22:44:28 +0200

    Add files via upload

---
+++ APBD-ASP.NET-CORE/FirstAspApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {// 
+     // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["ID"] = ID;
+
+            return View();
+        }
+    }
+}
---
+++ APBD-ASP.NET-CORE/FirstAspApp/Data/FirstAspAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAspApp.Models
+{
+    public class FirstAspAppContext : DbContext
+    {
+        public FirstAspAppContext (DbContextOptions<FirstAspAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstAspApp.Models.Movie> Movie { get; set; }
+    }
+}
